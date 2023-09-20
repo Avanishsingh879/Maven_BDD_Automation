e93a8ba5bd47badd5bbf4ce923762263a203ab58
unknown <avanishsingh879@gmail.com>
2023-09-20 23:46:46 +0530

    New Feature file added Date- 21-9-2023

--- src/test/java/Testrunner/TestSuiterunner.java
+++ src/test/java/Testrunner/TestSuiterunner.java
@@ -7,7 +7,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 		strict = true,
 		monochrome = true, 
 		features = {"src/test/resources/features/"},
-		tags={"@Tools_2"},
+		tags={"@Quick_1"},
 		glue = {"stepdefinition","Testrunner"}, 	
 		plugin = {
 	                "io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm",
