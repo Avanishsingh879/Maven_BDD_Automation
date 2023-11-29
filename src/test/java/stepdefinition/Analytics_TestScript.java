01052f176198b47b530230378d92101c8201488f
unknown <avanishsingh879@gmail.com>
2023-11-30 00:06:52 +0530

    Code updated Date: 30-11-2023

--- src/test/java/stepdefinition/Analytics_TestScript.java
+++ src/test/java/stepdefinition/Analytics_TestScript.java
@@ -3,7 +3,10 @@ package stepdefinition;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.maven.shared.utils.io.FileUtils;
@@ -14,6 +17,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 
 import Generic_Method.Utility_Method;
@@ -73,8 +77,8 @@ public class Analytics_TestScript {
 	public void user_has_click_on_Analytics_tab() throws InterruptedException {
 		
 		Thread.sleep(2000);
-		WebElement anylitcsBtn=driver.findElement(By.xpath("//a[text()='Analytics']"));
-		anylitcsBtn.click();
+		//WebElement anylitcsBtn=driver.findElement(By.xpath("//a[text()='Analytics']"));
+		//anylitcsBtn.click();
 		Utility_Method.captureScreenShot(driver);
 		Thread.sleep(2000);
 		
@@ -83,19 +87,85 @@ public class Analytics_TestScript {
 	}
 
 	@Then("User has click on Reports tab")
-	public void user_has_click_on_Reports_tab() {
-		
-	
+	public void user_has_click_on_Reports_tab() throws InterruptedException {
+		Thread.sleep(2000);
+		WebElement ReportsBtn=driver.findElement(By.xpath("//a[text()='Analytics']"));
+		Actions act=new Actions(driver);
+		act.moveToElement(ReportsBtn).build().perform();
+		Thread.sleep(2000);
+		WebElement ReportsBtnn=driver.findElement(By.xpath("//div[@id='Analytics_sub']//a[text()='Reports']"));
+		ReportsBtnn.click();
 		
 	    
 	}
 
 	@Then("User verify and click on new report in Analytics Page")
-	public void user_verify_and_click_on_new_report_in_Analytics_Page() {
+	public void user_verify_and_click_on_new_report_in_Analytics_Page() throws InterruptedException {
 		
-		
-		
+		//WebElement ReportsBtn=driver.findElement(By.xpath("//div[@id='Analytics_sub']//a[text()='Reports']"));
+		//ReportsBtn.click();
+	    driver.findElement(By.xpath("//img[@title='Create Report...']")).click();
+	    Thread.sleep(2000);
+	    List<WebElement>AllList=driver.findElements(By.xpath("//div[@id='reportLay']/table/tbody/tr[2]//a"));
 	    
+	    for(WebElement list:AllList) {
+	    	
+	    	if(list.getText().equals("- Accounts")) {
+	    		
+	    		list.click();
+	    		break;
+	    	}
+	    }
+	    	
+	     Thread.sleep(2000);
+	     String mainwindow=driver.getWindowHandle();
+	     System.out.println(mainwindow);
+	     Set<String>set=driver.getWindowHandles();
+	     Iterator<String>itr=set.iterator();
+	     while(itr.hasNext()==true) {
+	    	 
+	    	 String childwindow=itr.next();
+	    	 System.out.println(childwindow);
+	    	 String Title=driver.switchTo().window(childwindow).getTitle();
+	    	 System.out.println(Title);
+	    	 
+	    	 if(Title.equalsIgnoreCase("vtiger CRM - Create Report")) {
+	    		 
+	    		 break;
+	    	 }
+	     }
+	     System.out.println("Multiple window Handling");
+	     driver.manage().window().maximize();
+	     WebElement reprtname=driver.findElement(By.xpath("//input[@name='reportname']"));
+	     reprtname.sendKeys("Test");
+	     Thread.sleep(2000);
+	     List<WebElement>AllDropList=driver.findElements(By.xpath("//table[@class='small']//select/option"));
+	     //int size=AllDropList.size();
+	         //for(int i=0;i<=size-1;i++) {
+	        	
+	        	 //AllDropList.get(i).click();
+	        	 //Thread.sleep(2000);
+	         //}
+	         //System.out.println("All List Verify");
+	     
+	     for(WebElement List1:AllDropList) {
+	    	 
+	    	 if(List1.getText().equals("Activity Reports")) {
+	    		 
+	    		 List1.click();
+	    		 break;
+	    	 }
+	    	 
+	     }
+	      Thread.sleep(1000);
+	      JavascriptExecutor js1=(JavascriptExecutor)driver;
+	      WebElement textArea=driver.findElement(By.xpath("//textarea[@name='reportdes']"));
+	      js1.executeScript("arguments[0].value='Test';",textArea);
+	      Thread.sleep(1000);
+	      WebElement nextBtn=driver.findElement(By.xpath("//input[@id='next']"));
+	      //JavascriptExecutor js1=(JavascriptExecutor)driver;
+	      js1.executeScript("arguments[0].click()",nextBtn);
+	      Thread.sleep(2000);
 	}
 	
 	
