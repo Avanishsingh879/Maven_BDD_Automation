f0db8100213bd91ab1722bf4fab5ed8314b24112
unknown <avanishsingh879@gmail.com>
2023-12-17 23:29:54 +0530

    Update Groovy file Date: 17-12-2023

--- Test.groovy
+++ Test.groovy
@@ -1,51 +1,41 @@
-pipeline{
+pipeline {
+    agent any
 
-agent any
- stages{
-      stage(Get Source Code){
-      steps{
-      
-         git'https://github.com/Avanishsingh879/Maven_BDD_Automation.git'
-                echo 'Hello World''
-      }
-      }
-      stage('Build code'){
-      steps{
-      
-         bat script:'mvn complie'
-      }
-      }
-      
-      stage('Run Test'){
-      steps{
-       
-         bat script: 'mvn test -Dbrowser=localchrome'
-      
-      }
-      }
-      
-      stage('Publish Report'){
-      steps{
-      
-      allure includeProperties: false, jdk: '', results: [[path: 'allure-results']]
-      }
-      }
-      
-      stage('Email Notification'){
-      steps{
-      
-      steps{
+    stages {
+        stage('Get Source Code') {
+            steps {
+                git 'https://github.com/Avanishsingh879/Maven_BDD_Automation.git'
+                echo 'Hello World'
+            }
+        }
+        
+        stage('Build Code'){
+        steps{
+            
+            bat script:'mvn compile'
+        }
+        }
+        stage('Run Test'){
+        steps{
+            
+            bat script:'mvn test -Dbrowser=localchrome'
+        }
+        }
+        
+        stage('Publish report'){
+        steps{
+            
+          allure includeProperties: false, jdk: '', results: [[path: 'allure-results']]
+        }
+        }
+        stage('Email Notification'){
+            steps{
                 
                mail bcc: '', body: '''Hi Welcome to Jenkins Email Alerts
 
 Thanks
 Avanish''', cc: '', from: '', replyTo: '', subject: 'Jenkins Job', to: 'avanishsinghqa1@gmail.com'
-      
-      }
-      }
- 
- }
+            }
+        }
+    }
 }
-
-
-
