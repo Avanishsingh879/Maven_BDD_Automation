bbcc204dcab9b31a66d24f963a07458933deb92d
unknown <avanishsingh879@gmail.com>
2024-07-19 10:58:08 +0530

    Code updated date: 19-7-24

--- src/test/java/stepdefinition/FeedBack_TestScript.java
+++ src/test/java/stepdefinition/FeedBack_TestScript.java
@@ -29,7 +29,7 @@ public class FeedBack_TestScript {
 	
 	public static WebDriver driver;
 	public static Properties files;
-	//Test case passed
+	//Test case passed Date: 19-7-24
 	
 	@Given("User navigate the Application Url")
 	public void user_navigate_the_Application_Url() throws IOException, InterruptedException {
